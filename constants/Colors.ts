9b0bced28197faa3810ba2a4e1d708b3f32adde3
Faesel Saeed <faesel@gmail.com>
2021-02-18 17:44:56 +0000

    added light and dark color scheme

--- constants/Colors.ts
+++ constants/Colors.ts
@@ -38,3 +38,34 @@ export default {
     tabIconSelected: tintColorLight,
   },
 };
+
+
+// export default {
+//     light: {
+//       primary: '#6200EE',
+//       primaryVariant: '#3700B3',
+//       secondary: '#03DAC6',
+//       background: '#FFFFFF',
+//       error: '#B00020',
+
+//       onPrimary: '#FFFFFF',
+//       onSecondary: '#000000',
+//       onBackground: '#FFFFFF',
+//       onError: '#FFFFFF'
+//     },
+//     dark: {
+//       primary: '#BB86FC',
+//       primaryVariant: '#3700B3',
+//       secondary: '#03DAC6',
+//       background: '#121212',
+//       error: '#CF6679',
+      
+//       onPrimary: '#000000',
+//       onSecondary: '#000000',
+//       onBackground: '#000000',
+//       onError: '#000000'
+//     },
+//   };
+  
+  
+  
