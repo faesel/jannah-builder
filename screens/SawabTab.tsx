b2bb2482629dafb379f56350ffbd11d1a384bf5f
Faesel Saeed <faesel.saeed@plumguide.com>
2021-02-19 16:40:58 +0000

    updated sawab styling

--- screens/SawabTab.tsx
+++ screens/SawabTab.tsx
@@ -4,7 +4,7 @@ import { StyleSheet, TouchableOpacity } from 'react-native';
 import { Text, View } from '../components/Themed';
 import Colors from '../constants/Colors'
 import useColorScheme from '../hooks/useColorScheme';
-import { AntDesign } from '@expo/vector-icons';
+import { FontAwesome5, AntDesign, MaterialCommunityIcons, FontAwesome } from '@expo/vector-icons';
 import { RootStackParamList } from '../types';
 import { StackScreenProps } from '@react-navigation/stack';
 
@@ -34,13 +34,15 @@ export default function SawabTab({ navigation }: StackScreenProps<RootStackParam
               display: "flex",
               padding: 10
             }}>
-            <Text
-              lightColor={Colors[colorScheme].onSurface}
-              darkColor={Colors[colorScheme].onSurface}
-              style={{ fontSize: 20, fontWeight: 'bold', paddingBottom: 10 }}>
-              Prayers
-            </Text>
-
+              <View style={{flexDirection: "row", display: "flex"}} lightColor={Colors[colorScheme].surface} darkColor={Colors[colorScheme].surface}>
+                <FontAwesome5 name="pray" size={20} style={{ paddingRight: 10 }} color={Colors[colorScheme].onSurface} />  
+                <Text
+                  lightColor={Colors[colorScheme].onSurface}
+                  darkColor={Colors[colorScheme].onSurface}
+                  style={{ fontSize: 20, fontWeight: 'bold', paddingBottom: 10 }}>
+                  Prayers
+                </Text>
+              </View>
             <View
               lightColor={Colors[colorScheme].surface}
               darkColor={Colors[colorScheme].surface}
@@ -52,47 +54,47 @@ export default function SawabTab({ navigation }: StackScreenProps<RootStackParam
               <Text
                 lightColor={Colors[colorScheme].onSurface}
                 darkColor={Colors[colorScheme].onSurface}
-                style={{ paddingRight: 5 }}>
+                style={{ paddingRight: 5, fontSize: 15, fontWeight: "bold" }}>
                 Fajar
             </Text>
 
-              <AntDesign name="checkcircle" size={15} style={{ paddingRight: 10 }} color={Colors[colorScheme].secondary} />
+              <AntDesign name="checkcircle" size={20} style={{ paddingRight: 10 }} color={Colors[colorScheme].secondary} />
 
               <Text
                 lightColor={Colors[colorScheme].onSurface}
                 darkColor={Colors[colorScheme].onSurface}
-                style={{ paddingRight: 5 }}>
+                style={{ paddingRight: 5, fontSize: 15, fontWeight: "bold" }}>
                 Dhuhr
             </Text>
 
-              <AntDesign name="checkcircle" size={15} style={{ paddingRight: 10 }} color={Colors[colorScheme].secondary} />
+              <AntDesign name="checkcircle" size={20} style={{ paddingRight: 10 }} color={Colors[colorScheme].secondary} />
 
               <Text
                 lightColor={Colors[colorScheme].onSurface}
                 darkColor={Colors[colorScheme].onSurface}
-                style={{ paddingRight: 5 }}>
+                style={{ paddingRight: 5, fontSize: 15, fontWeight: "bold" }}>
                 Asr
             </Text>
 
-              <AntDesign name="checkcircle" size={15} style={{ paddingRight: 10 }} color="gray" />
+              <AntDesign name="checkcircle" size={20} style={{ paddingRight: 10 }} color="gray" />
 
               <Text
                 lightColor={Colors[colorScheme].onSurface}
                 darkColor={Colors[colorScheme].onSurface}
-                style={{ paddingRight: 5 }}>
+                style={{ paddingRight: 5, fontSize: 15, fontWeight: "bold" }}>
                 Maghrib
             </Text>
 
-              <AntDesign name="checkcircle" size={15} style={{ paddingRight: 10 }} color={Colors[colorScheme].secondary} />
+              <AntDesign name="checkcircle" size={20} style={{ paddingRight: 10 }} color={Colors[colorScheme].secondary} />
 
               <Text
                 lightColor={Colors[colorScheme].onSurface}
                 darkColor={Colors[colorScheme].onSurface}
-                style={{ paddingRight: 5 }}>
+                style={{ paddingRight: 5, fontSize: 15, fontWeight: "bold" }}>
                 Isha
             </Text>
 
-              <AntDesign name="checkcircle" size={15} style={{ paddingRight: 10 }} color={Colors[colorScheme].secondary} />
+              <AntDesign name="checkcircle" size={20} style={{ paddingRight: 10 }} color={Colors[colorScheme].secondary} />
 
 
             </View>
@@ -120,23 +122,23 @@ export default function SawabTab({ navigation }: StackScreenProps<RootStackParam
             display: "flex",
             padding: 10
           }}>
-            <Text
-              lightColor={Colors[colorScheme].onSurface}
-              darkColor={Colors[colorScheme].onSurface}
-              style={{ fontSize: 20, fontWeight: "bold" }}>
-              Quran
-            </Text>
+
+            <View style={{flexDirection: "row", display: "flex"}} lightColor={Colors[colorScheme].surface} darkColor={Colors[colorScheme].surface}>
+              <FontAwesome5 name="quran" size={20} style={{ paddingRight: 10 }} color={Colors[colorScheme].onSurface} />  
+              <Text
+                lightColor={Colors[colorScheme].onSurface}
+                darkColor={Colors[colorScheme].onSurface}
+                style={{ fontSize: 20, fontWeight: "bold" }}>
+                Quran
+              </Text>
+            </View>
 
           <View
             lightColor={Colors[colorScheme].secondary}
             darkColor={Colors[colorScheme].secondary}
-            style={{ width: 300, maxHeight: 40, minHeight: 40, marginTop:10 }}
-          >
-              <Text
-                style={{padding: 10}}
-              >50%</Text>
+            style={{ width: 300, maxHeight: 40, minHeight: 40, marginTop:10 }}>
+              <Text style={{padding: 10, fontWeight: "bold", fontSize: 15}}>50%</Text>
           </View>
-
 
         </View>
 
@@ -161,28 +163,98 @@ export default function SawabTab({ navigation }: StackScreenProps<RootStackParam
             display: "flex",
             padding: 10
           }}>
-          <Text
-            lightColor={Colors[colorScheme].onSurface}
-            darkColor={Colors[colorScheme].onSurface}
-            style={{ fontSize: 20, fontWeight: "bold" }}>
-            Zakat
-            </Text>
+             <View style={{flexDirection: "row", display: "flex"}} lightColor={Colors[colorScheme].surface} darkColor={Colors[colorScheme].surface}>
+                <FontAwesome5 name="money-bill-alt" size={20} style={{ paddingRight: 10 }} color={Colors[colorScheme].onSurface} />  
+                <Text
+                  lightColor={Colors[colorScheme].onSurface}
+                  darkColor={Colors[colorScheme].onSurface}
+                  style={{ fontSize: 20, fontWeight: "bold" }}>
+                  Zakat
+                </Text>
+              </View>
+
+            <View style={{ flexDirection: "row", display: "flex", marginTop: 10 }} lightColor={Colors[colorScheme].surface} darkColor={Colors[colorScheme].surface}>
+            <Text lightColor={Colors[colorScheme].onSurface} darkColor={Colors[colorScheme].onSurface} style={{ fontSize: 25, fontWeight: "bold" }}>£30</Text>
+              <Text lightColor={Colors[colorScheme].onSurface} darkColor={Colors[colorScheme].onSurface} style={{ fontSize: 35, fontWeight: "bold" }}>/£100</Text>
+            </View>
+
+        </View>
+      </View>
+
+      <View
+        lightColor={Colors[colorScheme].surface}
+        darkColor={Colors[colorScheme].surface}
+        style={{
+          flexDirection: "column",
+          display: "flex",
+          margin: 10,
+          padding: 10,
+          borderRadius: 15,
+          marginTop: 0
+        }}>
+        <View
+          lightColor={Colors[colorScheme].surface}
+          darkColor={Colors[colorScheme].surface}
+          style={{
+            flexDirection: "column",
+            display: "flex",
+            padding: 10
+          }}>
+             <View style={{flexDirection: "row", display: "flex"}} lightColor={Colors[colorScheme].surface} darkColor={Colors[colorScheme].surface}>
+                <MaterialCommunityIcons name="brain" size={20} style={{ paddingRight: 10 }} color={Colors[colorScheme].onSurface} />
+                <Text
+                  lightColor={Colors[colorScheme].onSurface}
+                  darkColor={Colors[colorScheme].onSurface}
+                  style={{ fontSize: 20, fontWeight: "bold" }}>
+                  Dhirk
+                </Text>
+              </View>
 
             <View
-            lightColor={Colors[colorScheme].secondary}
-            darkColor={Colors[colorScheme].secondary}
-            style={{ width: 200, maxHeight: 40, minHeight: 40, marginTop:10 }}
-          >
-              <Text
-                style={{padding: 10}}
-              >$30</Text>
-          </View>
-
-
+              lightColor={Colors[colorScheme].secondary}
+              darkColor={Colors[colorScheme].secondary}
+              style={{ width: 200, maxHeight: 40, minHeight: 40, marginTop:10 }}>
+                <Text style={{padding: 10, fontWeight: "bold", fontSize: 15 }}>30 Min</Text>
+            </View>
         </View>
       </View>
 
+      <View
+        lightColor={Colors[colorScheme].surface}
+        darkColor={Colors[colorScheme].surface}
+        style={{
+          flexDirection: "column",
+          display: "flex",
+          margin: 10,
+          padding: 10,
+          borderRadius: 15,
+          marginTop: 0
+        }}>
+        <View
+          lightColor={Colors[colorScheme].surface}
+          darkColor={Colors[colorScheme].surface}
+          style={{
+            flexDirection: "column",
+            display: "flex",
+            padding: 10
+          }}>
+             <View style={{flexDirection: "row", display: "flex"}} lightColor={Colors[colorScheme].surface} darkColor={Colors[colorScheme].surface}>
+              <FontAwesome name="handshake-o" size={20} style={{ paddingRight: 10 }} color={Colors[colorScheme].onSurface} />
+                <Text
+                  lightColor={Colors[colorScheme].onSurface}
+                  darkColor={Colors[colorScheme].onSurface}
+                  style={{ fontSize: 20, fontWeight: "bold" }}>
+                    Good Deeds
+                </Text>
+              </View>
 
+              <View style={{ flexDirection: "row", display: "flex", marginTop: 10 }} lightColor={Colors[colorScheme].surface} darkColor={Colors[colorScheme].surface}>
+              <Text lightColor={Colors[colorScheme].onSurface} darkColor={Colors[colorScheme].onSurface} style={{ fontSize: 25, fontWeight: "bold" }}>3</Text>
+                <Text lightColor={Colors[colorScheme].onSurface} darkColor={Colors[colorScheme].onSurface} style={{ fontSize: 35, fontWeight: "bold" }}>/10</Text>
+              </View>
+           
+        </View>
+      </View>
 
       <View
         lightColor={Colors[colorScheme].surface}
@@ -207,7 +279,7 @@ export default function SawabTab({ navigation }: StackScreenProps<RootStackParam
             lightColor={Colors[colorScheme].onSurface}
             darkColor={Colors[colorScheme].onSurface}
             style={{ fontSize: 20, fontWeight: "bold" }}>
-            Total: 100
+            Sawab Summary: 100 Sawab
             </Text>
 
         </View>
