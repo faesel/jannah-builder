c7d0c4b8d807c36eb177947713295040a0f33c85
Faesel Saeed <faesel@gmail.com>
2021-02-19 08:35:47 +0000

    cleaned up panels

--- screens/SawabTab.tsx
+++ screens/SawabTab.tsx
@@ -8,130 +8,97 @@ import { AntDesign } from '@expo/vector-icons';
 import { RootStackParamList } from '../types';
 import { StackScreenProps } from '@react-navigation/stack';
 
-import {
-  LineChart,
-  BarChart,
-  PieChart,
-  ProgressChart,
-  ContributionGraph,
-  StackedBarChart
-} from "react-native-chart-kit";
-import { Dimensions } from 'react-native';
-import { AbstractChartConfig } from 'react-native-chart-kit/dist/AbstractChart';
-
 export default function SawabTab({ navigation }: StackScreenProps<RootStackParamList>) {
   const colorScheme = useColorScheme();
-
-  const commitsData = [
-    { date: "2017-01-02", count: 1 },
-    { date: "2017-01-03", count: 2 },
-    { date: "2017-01-04", count: 3 },
-    { date: "2017-01-05", count: 4 },
-    { date: "2017-01-06", count: 5 },
-    { date: "2017-01-30", count: 2 },
-    { date: "2017-01-31", count: 3 },
-    { date: "2017-03-01", count: 2 },
-    { date: "2017-04-02", count: 4 },
-    { date: "2017-03-05", count: 2 },
-    { date: "2017-02-30", count: 4 }
-  ];
-  
-
-  const data = {
-    labels: ["Prayer", "Quran", "Zakat"], // optional
-    data: [0.4, 0.6, 0.8],
-    colors: ["#dfe4ea", "#ced6e0", "#a4b0be"]
-  };
-  const screenWidth = Dimensions.get("window").width;
 
   return (
     <View style={styles.container}>
 
-<TouchableOpacity onPress={() => navigation.navigate('Prayer')}>
-      <View
-        lightColor={Colors[colorScheme].surface}
-        darkColor={Colors[colorScheme].surface}
-        style={{
-          flexDirection: "column",
-          display: "flex",
-          margin: 10,
-          borderRadius: 15,
-          padding: 10
-        }}>
-
+      <TouchableOpacity onPress={() => navigation.navigate('Prayer')}>
         <View
           lightColor={Colors[colorScheme].surface}
           darkColor={Colors[colorScheme].surface}
           style={{
             flexDirection: "column",
             display: "flex",
+            margin: 10,
+            borderRadius: 15,
             padding: 10
           }}>
-          <Text
-            lightColor={Colors[colorScheme].onSurface}
-            darkColor={Colors[colorScheme].onSurface}
-            style={{ fontSize: 20, fontWeight: 'bold', paddingBottom: 10 }}>
-              Prayers
-            </Text>
 
           <View
             lightColor={Colors[colorScheme].surface}
             darkColor={Colors[colorScheme].surface}
             style={{
-              flexDirection: "row",
+              flexDirection: "column",
               display: "flex",
+              padding: 10
             }}>
-
             <Text
               lightColor={Colors[colorScheme].onSurface}
               darkColor={Colors[colorScheme].onSurface}
-              style={{ paddingRight: 5 }}>
-              Fajar
+              style={{ fontSize: 20, fontWeight: 'bold', paddingBottom: 10 }}>
+              Prayers
             </Text>
 
-            <AntDesign name="checkcircle" size={15} style={{ paddingRight: 10 }} color={Colors[colorScheme].secondary} />
+            <View
+              lightColor={Colors[colorScheme].surface}
+              darkColor={Colors[colorScheme].surface}
+              style={{
+                flexDirection: "row",
+                display: "flex",
+              }}>
 
-            <Text
-              lightColor={Colors[colorScheme].onSurface}
-              darkColor={Colors[colorScheme].onSurface}
-              style={{ paddingRight: 5 }}>
-              Dhuhr
+              <Text
+                lightColor={Colors[colorScheme].onSurface}
+                darkColor={Colors[colorScheme].onSurface}
+                style={{ paddingRight: 5 }}>
+                Fajar
             </Text>
 
-            <AntDesign name="checkcircle" size={15} style={{ paddingRight: 10 }} color={Colors[colorScheme].secondary} />
+              <AntDesign name="checkcircle" size={15} style={{ paddingRight: 10 }} color={Colors[colorScheme].secondary} />
 
-            <Text
-              lightColor={Colors[colorScheme].onSurface}
-              darkColor={Colors[colorScheme].onSurface}
-              style={{ paddingRight: 5 }}>
-              Asr
+              <Text
+                lightColor={Colors[colorScheme].onSurface}
+                darkColor={Colors[colorScheme].onSurface}
+                style={{ paddingRight: 5 }}>
+                Dhuhr
             </Text>
 
-            <AntDesign name="checkcircle" size={15} style={{ paddingRight: 10 }} color="gray" />
+              <AntDesign name="checkcircle" size={15} style={{ paddingRight: 10 }} color={Colors[colorScheme].secondary} />
 
-            <Text
-              lightColor={Colors[colorScheme].onSurface}
-              darkColor={Colors[colorScheme].onSurface}
-              style={{ paddingRight: 5 }}>
-              Maghrib
+              <Text
+                lightColor={Colors[colorScheme].onSurface}
+                darkColor={Colors[colorScheme].onSurface}
+                style={{ paddingRight: 5 }}>
+                Asr
             </Text>
 
-            <AntDesign name="checkcircle" size={15} style={{ paddingRight: 10 }} color={Colors[colorScheme].secondary} />
+              <AntDesign name="checkcircle" size={15} style={{ paddingRight: 10 }} color="gray" />
 
-            <Text
-              lightColor={Colors[colorScheme].onSurface}
-              darkColor={Colors[colorScheme].onSurface}
-              style={{ paddingRight: 5 }}>
-              Isha
+              <Text
+                lightColor={Colors[colorScheme].onSurface}
+                darkColor={Colors[colorScheme].onSurface}
+                style={{ paddingRight: 5 }}>
+                Maghrib
             </Text>
 
-            <AntDesign name="checkcircle" size={15} style={{ paddingRight: 10 }} color={Colors[colorScheme].secondary} />
+              <AntDesign name="checkcircle" size={15} style={{ paddingRight: 10 }} color={Colors[colorScheme].secondary} />
 
-           
+              <Text
+                lightColor={Colors[colorScheme].onSurface}
+                darkColor={Colors[colorScheme].onSurface}
+                style={{ paddingRight: 5 }}>
+                Isha
+            </Text>
+
+              <AntDesign name="checkcircle" size={15} style={{ paddingRight: 10 }} color={Colors[colorScheme].secondary} />
+
+
+            </View>
           </View>
-        </View>
 
-      </View>
+        </View>
       </TouchableOpacity>
 
       <View
@@ -145,7 +112,7 @@ export default function SawabTab({ navigation }: StackScreenProps<RootStackParam
           padding: 10,
           marginTop: 0,
         }}>
-              <View
+        <View
           lightColor={Colors[colorScheme].surface}
           darkColor={Colors[colorScheme].surface}
           style={{
@@ -160,7 +127,18 @@ export default function SawabTab({ navigation }: StackScreenProps<RootStackParam
               Quran
             </Text>
 
+          <View
+            lightColor={Colors[colorScheme].primary}
+            darkColor={Colors[colorScheme].primary}
+            style={{ width: 300, maxHeight: 40, minHeight: 40, marginTop:10 }}
+          >
+              <Text
+                style={{padding: 10}}
+              >50%</Text>
           </View>
+
+
+        </View>
 
       </View>
 
@@ -175,7 +153,7 @@ export default function SawabTab({ navigation }: StackScreenProps<RootStackParam
           borderRadius: 15,
           marginTop: 0
         }}>
-           <View
+        <View
           lightColor={Colors[colorScheme].surface}
           darkColor={Colors[colorScheme].surface}
           style={{
@@ -183,14 +161,25 @@ export default function SawabTab({ navigation }: StackScreenProps<RootStackParam
             display: "flex",
             padding: 10
           }}>
-            <Text
-              lightColor={Colors[colorScheme].onSurface}
-              darkColor={Colors[colorScheme].onSurface}
-              style={{ fontSize: 20, fontWeight: "bold" }}>
-              Zakat
+          <Text
+            lightColor={Colors[colorScheme].onSurface}
+            darkColor={Colors[colorScheme].onSurface}
+            style={{ fontSize: 20, fontWeight: "bold" }}>
+            Zakat
             </Text>
 
+            <View
+            lightColor={Colors[colorScheme].primary}
+            darkColor={Colors[colorScheme].primary}
+            style={{ width: 200, maxHeight: 40, minHeight: 40, marginTop:10 }}
+          >
+              <Text
+                style={{padding: 10}}
+              >$30</Text>
           </View>
+
+
+        </View>
       </View>
 
 
@@ -206,7 +195,7 @@ export default function SawabTab({ navigation }: StackScreenProps<RootStackParam
           borderRadius: 15,
           marginTop: 0
         }}>
-           <View
+        <View
           lightColor={Colors[colorScheme].surface}
           darkColor={Colors[colorScheme].surface}
           style={{
@@ -214,57 +203,15 @@ export default function SawabTab({ navigation }: StackScreenProps<RootStackParam
             display: "flex",
             padding: 10
           }}>
-            <Text
-              lightColor={Colors[colorScheme].onSurface}
-              darkColor={Colors[colorScheme].onSurface}
-              style={{ fontSize: 20, fontWeight: "bold" }}>
-              Total: 100 
+          <Text
+            lightColor={Colors[colorScheme].onSurface}
+            darkColor={Colors[colorScheme].onSurface}
+            style={{ fontSize: 20, fontWeight: "bold" }}>
+            Total: 100
             </Text>
 
-          </View>
+        </View>
       </View>
-
-
-      <View
-        style={{
-          flexDirection: "column",
-          display: "flex",
-          margin: 20,
-          marginTop: 0
-        }}>
-          
-         
-
-<ContributionGraph
-  values={commitsData}
-  endDate={new Date("2020-04-01")}
-  numDays={105}
-  width={screenWidth}
-  height={220}
-  chartConfig={{
-    backgroundColor: Colors.light.background,
-    backgroundGradientFrom: Colors.light.background,
-    backgroundGradientTo: Colors.light.background,
-
-    decimalPlaces: 2, // optional, defaults to 2dp
-    color: (opacity = 1) => `rgba(3,218,198, ${opacity})`,
-    labelColor: (opacity = 1) => `rgba(0, 0, 0, ${opacity})`,
-    style: {
-      borderRadius: 16
-    },
-    propsForDots: {
-      r: "6",
-      strokeWidth: "2",
-      stroke: Colors.dark.primary
-    }
-  }}
-/>
-
-
-
-      </View>
-
-     
 
     </View>
   );
